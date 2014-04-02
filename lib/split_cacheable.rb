77b2c1504c8daf0f09d5b9093a296c9e89ea852f
Daniel Schwartz <dschwartz88@gmail.com>
2014-04-01 20:30:56 -0400

    removing pry and making only allowing the engine to bind on Rails 3

--- lib/split_cacheable.rb
+++ lib/split_cacheable.rb
@@ -1,7 +1,6 @@
-require 'pry'
 require "split_cacheable/version"
 require 'split_cacheable/helper'
-require 'split_cacheable/engine' if defined?(Rails) && Rails::VERSION::MAJOR >= 3
+require 'split_cacheable/engine' if defined?(Rails) && Rails::VERSION::MAJOR === 3
 require 'split'
 
 module Split
--- split_cacheable.gemspec
+++ split_cacheable.gemspec
@@ -20,8 +20,6 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "split"
   spec.add_dependency "activesupport"
-  spec.add_dependency "pry"
-  spec.add_dependency "pry-byebug"
 
   spec.add_development_dependency "bundler", "~> 1.5"
   spec.add_development_dependency "rake"
