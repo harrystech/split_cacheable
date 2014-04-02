d5453c37f6dad1d0e45dfaaa0825103bf0140411
Daniel Schwartz <dschwartz88@gmail.com>
2014-04-01 20:50:49 -0400

    add comments

--- lib/split_cacheable.rb
+++ lib/split_cacheable.rb
@@ -3,6 +3,13 @@ require 'split_cacheable/helper'
 require 'split_cacheable/engine' if defined?(Rails) && Rails::VERSION::MAJOR === 3
 require 'split'
 
+# This is the main Adapter instance. It expects:
+#     controller_instance => An instance of ActionController::Base
+#     action_name => The current action being performed (as a symbol)
+#
+# We don't include this as a module so that you can instantite this yourself
+# so you can call get_all_possible_variations to clear your caches
+
 module Split
     module Cacheable
         class Adapter
@@ -13,6 +20,7 @@ module Split
                 @action_name = action_name
             end
 
+            # Get all tests which should be active on this controller for this action
             def active_tests
                 ab_tests = @controller.class.split_cacheable_ab_tests.select { |test_obj|
                     is_active = false
@@ -25,6 +33,9 @@ module Split
                         is_active = true
                     end
 
+                    # The assumption here is that we should only evaluate the :if Proc or Boolean
+                    # if we are part of a live ActionController::Base.
+                    # This allows active_tests to return all possible active tests for when you call get_all_possible_variations
                     if @controller.request && test_obj[:if]
                         if is_active && (test_obj[:if].is_a?(Proc) ? test_obj[:if].call(@controller) : !!test_obj[:if])
                             is_active = true
@@ -37,6 +48,11 @@ module Split
                 }
             end
 
+            # Use Split to return a partial cache key (used in fragment_cache_key)
+            # by calling ab_test which is an internal Split::Helper method that is now on your
+            # controller instance
+            #
+            # You should not be calling this method outside of a live ActionController::Base
             def get_current_variations
                 if !@controller.request
                     return DEFAULT_KEY
@@ -45,6 +61,10 @@ module Split
                 end
             end
 
+            # Search the Split::ExperimentCatalog to find all tests and generate
+            # every possible partial cache key
+            #
+            # Use this to clear all your action_caches
             def get_all_possible_variations
                 test_variations = Array.new
                 active_tests.each { |test_obj|  
--- lib/split_cacheable/helper.rb
+++ lib/split_cacheable/helper.rb
@@ -7,16 +7,21 @@ module Split
             end
 
             module InstanceMethods
+                # Override the default ActionController::Base action_cache key creation
+                # by injecting 
                 def fragment_cache_key(key)
                     super("#{current_tests_and_variations}/#{key}")
                 end
 
+                # Controller helper method to get the current active tests + variations in the form of a cache_key
                 def current_tests_and_variations
                     Split::Cacheable::Adapter.new(self, self.action_name.to_sym).get_current_variations
                 end
             end
 
             module ClassMethods
+                # This is how you specify your tests in the sub-class of ActionController::Base
+                # ex: cacheable_ab_test :homepage_hero, :only => :our_story, :if => Rails.env.production?
                 def cacheable_ab_test(test_name, options)
                     options[:except] = Array(options[:except])
                     options[:only] = Array(options[:only])
@@ -24,6 +29,7 @@ module Split
                     self.split_cacheable_ab_tests << options.merge({:test_name => test_name})
                 end
 
+                # Class level variable. cacheable_ab_test's get pushed onto it
                 def split_cacheable_ab_tests
                     @split_cacheable_ab_tests ||= Array.new
                 end
